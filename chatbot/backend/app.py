5ae9c84420a2453f77073dead3c2df467cece36f
Lina Gabrielly Fernandes De Sousa <125388688+LINAGABRIELLY186@users.noreply.github.com>
2025-07-07 19:32:09 -0300

    Update app.py

--- chatbot/backend/app.py
+++ chatbot/backend/app.py
@@ -87,6 +87,7 @@ from flask import send_from_directory
 def homepage():
     return send_from_directory("static", "index.html")
 
-# Iniciar o servidor
 if __name__ == "__main__":
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
